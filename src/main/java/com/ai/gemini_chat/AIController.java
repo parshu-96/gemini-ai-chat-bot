e70b3ca3020a22f1236d47f38f7ec6e565c63fce
parshu <parshuram.m@accops.com>
2025-04-01 19:42:59 +0530

    Mapped method to post

--- src/main/java/com/ai/gemini_chat/AIController.java
+++ src/main/java/com/ai/gemini_chat/AIController.java
@@ -3,6 +3,7 @@ package com.ai.gemini_chat;
 import lombok.AllArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,8 @@ import java.util.Map;
 public class AIController {
 
     private final QnAService qnAService;
+
+    @PostMapping("/ask")
     public ResponseEntity<String> askQuestion(@RequestBody Map<String,String> payload){
          String question = payload.get("question");
          String answer=qnAService.getAnswer(question);
